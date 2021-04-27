04abcbd8a4e9c7761c12f7bfb124e6bb4bda84e2
alexander-chemeza <alexander.chemeza@gmail.com>
2021-04-27 15:55:33 +0300

    sidebar logged user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ import {RecoverComponent} from './pages/recover/recover.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {HomeComponent} from './pages/home/home.component';
 import {CalculatorComponent} from './pages/calculator/calculator.component';
+import {OrderComponent} from './pages/order/order.component';
+import {JournalComponent} from './pages/journal/journal.component';
+import {TemplateComponent} from './pages/template/template.component';
+import {BookComponent} from './pages/book/book.component';
+import {UsersComponent} from './pages/users/users.component';
+import {DocumentsComponent} from './pages/documents/documents.component';
+import {InstructionComponent} from './pages/instruction/instruction.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -12,7 +20,15 @@ const routes: Routes = [
   {path: 'recover', component: RecoverComponent},
   {path: 'registration', component: RegisterComponent},
   {path: 'calculator', component: CalculatorComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'order', component: OrderComponent},
+  {path: 'journal', component: JournalComponent},
+  {path: 'template', component: TemplateComponent},
+  {path: 'book', component: BookComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'documents', component: DocumentsComponent},
+  {path: 'instruction', component: InstructionComponent},
+  {path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { RegisterComponent } from './pages/register/register.component';
 import { CalculatorComponent } from './pages/calculator/calculator.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { JournalComponent } from './pages/journal/journal.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { BookComponent } from './pages/book/book.component';
+import { UsersComponent } from './pages/users/users.component';
+import { DocumentsComponent } from './pages/documents/documents.component';
+import { InstructionComponent } from './pages/instruction/instruction.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { OrderComponent } from './pages/order/order.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,15 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     RegisterComponent,
     CalculatorComponent,
     HomeComponent,
-    SidebarComponent
+    SidebarComponent,
+    JournalComponent,
+    TemplateComponent,
+    BookComponent,
+    UsersComponent,
+    DocumentsComponent,
+    InstructionComponent,
+    ProfileComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
