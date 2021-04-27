62895316ee618d08caebd155f7eaafff2de6a20b
alexander-chemeza <alexander.chemeza@gmail.com>
2021-04-27 14:27:10 +0300

    sidebar unlogged user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './pages/login/login.component';
+import {RecoverComponent} from './pages/recover/recover.component';
+import {RegisterComponent} from './pages/register/register.component';
+import {HomeComponent} from './pages/home/home.component';
+import {CalculatorComponent} from './pages/calculator/calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'recover', component: RecoverComponent},
+  {path: 'registration', component: RegisterComponent},
+  {path: 'calculator', component: CalculatorComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RecoverComponent } from './pages/recover/recover.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RecoverComponent,
+    RegisterComponent,
+    CalculatorComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
