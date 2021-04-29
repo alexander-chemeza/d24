d7bcf3f80ccea25df475c24b93819152752fb1a0
alexander-chemeza <alexander.chemeza@gmail.com>
2021-04-29 13:34:58 +0300

    Redundant comments

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  // EntityWithAgreement,
-  // EntityWithoutAgreement,
-  // IndividualWithAgreement,
-  // IndividualWithoutAgreement,
-  RestapiService, UserRegistration
+  RestapiService,
+  UserRegistration
 } from '../../restapi.service';
 import {Router} from '@angular/router';
 
@@ -75,15 +72,6 @@ export class RegisterComponent implements OnInit {
     let data: UserRegistration = {} as any;
     if (!this.agreement && !this.personWithAgreement) {
       data = {
-        // companyName: this.companyNameEntityWithAgreement,
-        // email: this.emailEntityWithAgreement,
-        // login: this.loginEntityWithAgreement,
-        // password: this.passwordEntityWithAgreement,
-        // phone: this.phoneEntityWithAgreement,
-        // unp: this.unpEntityWithAgreement,
-        // userName: this.userNameEntityWithAgreement
-
-        // passportNumber: '',
         userName: this.userNameEntityWithAgreement,
         email: this.emailEntityWithAgreement,
         phone: this.phoneEntityWithAgreement,
@@ -91,61 +79,12 @@ export class RegisterComponent implements OnInit {
         password: this.passwordEntityWithAgreement,
         companyName: this.companyNameEntityWithAgreement,
         unp: this.unpEntityWithAgreement,
-        // documentType: '',
-        // passportSeries: '',
-        // issueAddress: '',
-        // issueDate: '',
-        // personalNumber: '',
-        // homeAddress: '',
-        // deliveryFrequency: '',
-        // cargoDescription: '',
-        // clientName: '',
-        // legalAddress: '',
-        // postAddress: '',
-        // okpo: '',
-        // authorizedPerson: '',
-        // groundsForSigning: '',
-        // bankName: '',
-        // bankAddress: '',
-        // bankCode: '',
-        // account: '',
-        // settlementPerson: '',
-        // settlementPersonEmail: ''
       };
     } else if (this.agreement && !this.personWithoutAgreement) {
       data = {
-        // clientName_EntityWithoutAgreement: this.clientNameEntityWithoutAgreement,
-        // legalAddress_EntityWithoutAgreement: this.legalAddressEntityWithoutAgreement,
-        // postAddress_EntityWithoutAgreement: this.postAddressEntityWithoutAgreement,
-        // unp_EntityWithoutAgreement: this.unpEntityWithoutAgreement,
-        // okpo_EntityWithoutAgreement: this.okpoEntityWithoutAgreement,
-        // authorizedPerson_EntityWithoutAgreement: this.authorizedPersonEntityWithoutAgreement,
-        // groundsForSigning_EntityWithoutAgreement: this.groundsForSigningEntityWithoutAgreement,
-        // phone_EntityWithoutAgreement: this.phoneEntityWithoutAgreement,
-        // email_EntityWithoutAgreement: this.emailEntityWithoutAgreement,
-        // bankName_EntityWithoutAgreement: this.bankAddressEntityWithoutAgreement,
-        // bankAddress_EntityWithoutAgreement: this.bankAddressEntityWithoutAgreement,
-        // bankCode_EntityWithoutAgreement: this.bankCodeEntityWithoutAgreement,
-        // account_EntityWithoutAgreement: this.accountEntityWithoutAgreement,
-        // deliveryFrequency_EntityWithoutAgreement: this.deliveryFrequencyEntityWithoutAgreement,
-        // cargoDescription_EntityWithoutAgreement: this.cargoDescriptionEntityWithoutAgreement,
-        // settlementPerson_EntityWithoutAgreement: this.settlementPersonEntityWithoutAgreement,
-        // settlementPersonEmail_EntityWithoutAgreement: this.settlementPersonEmailEntityWithoutAgreement
-
-        // passportNumber: '',
-        // userName: '',
         email: this.emailEntityWithoutAgreement,
         phone: this.phoneEntityWithoutAgreement,
-        // login: '',
-        // password: '',
-        // companyName: '',
         unp: this.unpEntityWithoutAgreement,
-        // documentType: '',
-        // passportSeries: '',
-        // issueAddress: '',
-        // issueDate: '',
-        // personalNumber: '',
-        // homeAddress: '',
         deliveryFrequency: this.accountEntityWithoutAgreement,
         cargoDescription: this.settlementPersonEmailEntityWithoutAgreement,
         clientName: this.clientNameEntityWithoutAgreement,
@@ -163,65 +102,19 @@ export class RegisterComponent implements OnInit {
       };
     } else if (!this.agreement && this.personWithAgreement) {
       data = {
-        // passportNumber_IndividualWithAgreement: this.passportNumberIndividualWithAgreement,
-        // name_IndividualWithAgreement: this.nameIndividualWithAgreement,
-        // email_IndividualWithAgreement: this.emailIndividualWithAgreement,
-        // phone_IndividualWithAgreement: this.phoneIndividualWithAgreement,
-        // login_IndividualWithAgreement: this.loginIndividualWithAgreement,
-        // password_IndividualWithAgreement: this.passwordIndividualWithAgreement
-
         passportNumber: this.passportNumberIndividualWithAgreement,
         userName: this.nameIndividualWithAgreement,
         email: this.emailIndividualWithAgreement,
         phone: this.phoneIndividualWithAgreement,
         login: this.loginIndividualWithAgreement,
         password: this.passwordIndividualWithAgreement,
-        // companyName: '',
-        // unp: '',
-        // documentType: '',
-        // passportSeries: '',
-        // issueAddress: '',
-        // issueDate: '',
-        // personalNumber: '',
-        // homeAddress: '',
-        // deliveryFrequency: '',
-        // cargoDescription: '',
-        // clientName: '',
-        // legalAddress: '',
-        // postAddress: '',
-        // okpo: '',
-        // authorizedPerson: '',
-        // groundsForSigning: '',
-        // bankName: '',
-        // bankAddress: '',
-        // bankCode: '',
-        // account: '',
-        // settlementPerson: '',
-        // settlementPersonEmail: '',
       };
     } else if (this.agreement && this.personWithoutAgreement) {
       data = {
-        // name_IndividualWithoutAgreement: this.nameIndividualWithoutAgreement,
-        // documentType_IndividualWithoutAgreement: this.documentTypeIndividualWithoutAgreement,
-        // passportSeries_IndividualWithoutAgreement: this.passportSeriesIndividualWithoutAgreement,
-        // passportNumber_IndividualWithoutAgreement: this.passportNumberIndividualWithoutAgreement,
-        // issueAddress_IndividualWithoutAgreement: this.issueAddressIndividualWithoutAgreement,
-        // issueDate_IndividualWithoutAgreement: this.issueDateIndividualWithoutAgreement,
-        // personalNumber_IndividualWithoutAgreement: this.personalNumberIndividualWithoutAgreement,
-        // homeAddress_IndividualWithoutAgreement: this.homeAddressIndividualWithoutAgreement,
-        // phone_IndividualWithoutAgreement: this.phoneIndividualWithoutAgreement,
-        // email_IndividualWithoutAgreement: this.emailIndividualWithoutAgreement,
-        // deliveryFrequency_IndividualWithoutAgreement: this.deliveryFrequencyIndividualWithoutAgreement,
-        // cargoDescription_IndividualWithoutAgreement: this.cargoDescriptionIndividualWithoutAgreement
-
         passportNumber: this.passportNumberIndividualWithoutAgreement,
         userName: this.nameIndividualWithoutAgreement,
         email: this.emailIndividualWithoutAgreement,
         phone: this.phoneIndividualWithoutAgreement,
-        // login: '',
-        // password: '',
-        // companyName: '',
-        // unp: '',
         documentType: this.documentTypeIndividualWithoutAgreement,
         passportSeries: this.passportSeriesIndividualWithoutAgreement,
         issueAddress: this.issueAddressIndividualWithoutAgreement,
@@ -230,21 +123,9 @@ export class RegisterComponent implements OnInit {
         homeAddress: this.homeAddressIndividualWithoutAgreement,
         deliveryFrequency: this.deliveryFrequencyIndividualWithoutAgreement,
         cargoDescription: this.cargoDescriptionIndividualWithoutAgreement,
-        // clientName: '',
-        // legalAddress: '',
-        // postAddress: '',
-        // okpo: '',
-        // authorizedPerson: '',
-        // groundsForSigning: '',
-        // bankName: '',
-        // bankAddress: '',
-        // bankCode: '',
-        // account: '',
-        // settlementPerson: '',
-        // settlementPersonEmail: ''
       };
     }
-    console.log(data); // Вот вывод в консоль идет
+    console.log(data);
     this.service.register(data)
       .subscribe(response => {
         console.log(response);
--- src/app/restapi.service.ts
+++ src/app/restapi.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
-// login user data interface
+// Login user data interface
 export interface User {
   name: string;
   role: string;
 }
 
+// Registration user data interface
 export interface UserRegistration {
   passportNumber?: string;
   userName?: string;
@@ -38,61 +39,6 @@ export interface UserRegistration {
   settlementPerson?: string;
   settlementPersonEmail?: string;
 }
-
-// Object for registration of individual with agreement
-// export interface IndividualWithAgreement {
-  // passportNumber_IndividualWithAgreement: string;
-  // name_IndividualWithAgreement: string;
-  // email_IndividualWithAgreement: string;
-  // phone_IndividualWithAgreement: string;
-  // login_IndividualWithAgreement: string;
-  // password_IndividualWithAgreement: string;
-// }
-// Object for registration of entity with agreement
-// export interface EntityWithAgreement {
-  // companyName: string;
-  // email: string;
-  // login: string;
-  // password: string;
-  // phone: string;
-  // unp: string;
-  // userName: string;
-// }
-// Object for registration of individual without agreement
-// export interface IndividualWithoutAgreement {
-  // name_IndividualWithoutAgreement: string;
-  // documentType_IndividualWithoutAgreement: string;
-  // passportSeries_IndividualWithoutAgreement: string;
-  // passportNumber_IndividualWithoutAgreement: string;
-  // issueAddress_IndividualWithoutAgreement: string;
-  // issueDate_IndividualWithoutAgreement: string;
-  // personalNumber_IndividualWithoutAgreement: string;
-  // homeAddress_IndividualWithoutAgreement: string;
-  // phone_IndividualWithoutAgreement: string;
-  // email_IndividualWithoutAgreement: string;
-  // deliveryFrequency_IndividualWithoutAgreement: string;
-  // cargoDescription_IndividualWithoutAgreement: string;
-// }
-// Object for registration of entity without agreement
-// export interface EntityWithoutAgreement {
-// clientName_EntityWithoutAgreement: string;
-// legalAddress_EntityWithoutAgreement: string;
-// postAddress_EntityWithoutAgreement: string;
-// unp_EntityWithoutAgreement: string;
-// okpo_EntityWithoutAgreement: string;
-// authorizedPerson_EntityWithoutAgreement: string;
-// groundsForSigning_EntityWithoutAgreement: string;
-// phone_EntityWithoutAgreement: string;
-// email_EntityWithoutAgreement: string;
-// bankName_EntityWithoutAgreement: string;
-// bankAddress_EntityWithoutAgreement: string;
-// bankCode_EntityWithoutAgreement: string;
-// account_EntityWithoutAgreement: string;
-// deliveryFrequency_EntityWithoutAgreement: string;
-// cargoDescription_EntityWithoutAgreement: string;
-// settlementPerson_EntityWithoutAgreement: string;
-// settlementPersonEmail_EntityWithoutAgreement: string;
-// }
 
 @Injectable({
   providedIn: 'root'
