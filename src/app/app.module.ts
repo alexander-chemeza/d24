21c9e60c369731c762a4c0023c2f1662bd8355d2
alexander-chemeza <alexander.chemeza@gmail.com>
2021-04-30 22:57:11 +0300

    jornal page ag grid fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './pages/calculator/calculator.component';
-import { JournalComponent } from './pages/journal/journal.component';
 import { TemplateComponent } from './pages/template/template.component';
 import { BookComponent } from './pages/book/book.component';
 import { UsersComponent } from './pages/users/users.component';
@@ -16,12 +15,12 @@ import {LoginModule} from './pages/login/login.module';
 import {RecoverModule} from './pages/recover/recover.module';
 import {RegisterModule} from './pages/register/register.module';
 import {HomeModule} from './pages/home/home.module';
+import {JournalModule} from './pages/journal/journal.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    JournalComponent,
     TemplateComponent,
     BookComponent,
     UsersComponent,
@@ -36,7 +35,8 @@ import {HomeModule} from './pages/home/home.module';
     LoginModule,
     RecoverModule,
     RegisterModule,
-    HomeModule
+    HomeModule,
+    JournalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/journal/journal.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SharedModule} from '../../shared/shared.module';
+import {AgGridModule} from 'ag-grid-angular';
+import {JournalComponent} from './journal.component';
+
+
+
+@NgModule({
+  declarations: [JournalComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AgGridModule.withComponents([])
+  ]
+})
+export class JournalModule { }
