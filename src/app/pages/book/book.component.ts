6d522e4156d8cca3689447546bdb17a6b8ab7c43
alexander-chemeza <alexander.chemeza@gmail.com>
2021-05-12 22:29:22 +0300

    book contragent modal scripts and validation

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-book',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book.component.scss']
 })
 export class BookComponent implements OnInit {
+  // Reactive forms
+  newContragent: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newContragent = new FormGroup({
+      type: new FormControl('', [
+        Validators.required
+      ]),
+      name: new FormControl('', [
+        Validators.required
+      ])
+    });
+  }
+
+  showModal(id: string): void {
+    const modal: any = document.getElementById(id);
+    modal.classList.remove('hide-modal');
+    modal.classList.add('show-modal');
+  }
+
+  hideModal(id: string): void {
+    const modal: any = document.getElementById(id);
+    modal.classList.add('hide-modal');
+    modal.classList.remove('show-modal');
+  }
+
+  createNewContragent(): void {
+    // Write something
   }
 
 }
