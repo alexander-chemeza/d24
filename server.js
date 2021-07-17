983019d1e591785db57582e02f29ba4ff3f2c9fb
alexander-chemeza <alexander.chemeza@gmail.com>
2021-07-17 22:12:32 +0300

    server config

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4200;
+
+app.use('/', express.static('dist/d24'));
+app.use('/login', express.static('dist/d24'));
+app.use('/recover', express.static('dist/d24'));
+app.use('/registration', express.static('dist/d24'));
+app.use('/calculator', express.static('dist/d24'));
+app.use('/home', express.static('dist/d24'));
+app.use('/order', express.static('dist/d24'));
+app.use('/journal', express.static('dist/d24'));
+app.use('/template', express.static('dist/d24'));
+app.use('/book', express.static('dist/d24'));
+app.use('/users', express.static('dist/d24'));
+app.use('/documents', express.static('dist/d24'));
+app.use('/instruction', express.static('dist/d24'));
+app.use('/profile', express.static('dist/d24'));
+
+
+app.listen(port, () => {
+    console.log('Listening port: ' + port);
+});
