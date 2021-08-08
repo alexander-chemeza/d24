34a6253c5c5f10ca4823907e6bec3e3c74998dac
alexander-chemeza <alexander.chemeza@gmail.com>
2021-08-08 16:28:41 +0300

    recovery modals and check

--- src/app/pages/recover/recover.component.ts
+++ src/app/pages/recover/recover.component.ts
@@ -10,6 +10,7 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class RecoverComponent implements OnInit {
   recover = false;
+  incorrect = false;
 
   getPassCodeForm = new FormGroup({
     login: new FormControl('', [
@@ -36,23 +37,62 @@ export class RecoverComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getPassCode(): void {
-    console.log('sending');
+  get login(): any {
+    return this.getPassCodeForm.get('login');
+  }
 
-    this.service.getPassCode(this.getPassCodeForm.value.login, this.getPassCodeForm.value.email).subscribe(response => {
-      if(response.status === 200) {
-        this.recover = !this.recover;
-      }
-    });
+  get email(): any {
+    return this.getPassCodeForm.get('email');
+  }
+
+  get password(): any {
+    return this.recoverForm.get('password');
+  }
+
+  get passCode(): any {
+    return this.recoverForm.get('passCode');
+  }
+
+  getPassCode(): void {
+    if (this.getPassCodeForm.value.login && this.getPassCodeForm.value.email) {
+      this.incorrect = false;
+      this.service.getPassCode(this.getPassCodeForm.value.login, this.getPassCodeForm.value.email).subscribe(response => {
+        if (response.status === 200) {
+          this.recover = !this.recover;
+        }
+      });
+    } else {
+      this.incorrect = true;
+    }
   }
 
   doRecover(): void {
-    const data = {passCode: this.recoverForm.value.passCode, password: this.recoverForm.value.password};
-    this.service.recover(data).subscribe(response => {
-      if(response.status === 200) {
-        this.router.navigate(['login']);
-      }
-    });
+    if (this.recoverForm.value.passCode && this.recoverForm.value.password) {
+      this.incorrect = false;
+      const data = {passCode: this.recoverForm.value.passCode, password: this.recoverForm.value.password};
+      this.service.recover(data).subscribe(response => {
+        if (response.status === 200) {
+          this.showModal('user-updated');
+          setTimeout(() => {
+            this.hideModal('user-updated');
+            this.router.navigate(['login']);
+          }, 3000);
+        }
+      });
+    } else {
+      this.incorrect = true;
+    }
   }
 
+  showModal(id: string): void {
+    const modal: any = document.getElementById(id);
+    modal.classList.remove('hide-modal');
+    modal.classList.add('show-modal');
+  }
+
+  hideModal(id: string): void {
+    const modal: any = document.getElementById(id);
+    modal.classList.add('hide-modal');
+    modal.classList.remove('show-modal');
+  }
 }
