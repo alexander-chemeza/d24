e700793aea3ace5769c89caa7425a58b72635c34
alexander-chemeza <alexander.chemeza@gmail.com>
2021-08-22 09:10:59 +0300

    basic auth fix

--- src/app/AuthInterceptor.ts
+++ src/app/AuthInterceptor.ts
@@ -14,6 +14,7 @@ export class AuthInterceptor implements HttpInterceptor {
     request = request.clone({
       setHeaders: {
         'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
       },
       withCredentials: true
     });
