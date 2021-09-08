106bc43a844e3b05db94926728cbfa9c4e85c551
alexander-chemeza <alexander.chemeza@gmail.com>
2021-09-08 21:59:54 +0300

    modals to block user

--- src/app/pages/users/user-block-controll/user-block-controll.component.ts
+++ src/app/pages/users/user-block-controll/user-block-controll.component.ts
@@ -8,13 +8,27 @@ import {ICellRendererParams} from 'ag-grid-community';
   styleUrls: ['./user-block-controll.component.scss']
 })
 export class UserBlockControllComponent implements ICellRendererAngularComp, OnDestroy {
+  state = false;
   private params: any;
+  cells: any = document.getElementsByClassName('ag-cell');
 
   agInit(params: any): void {
     this.params = params;
   }
 
   btnClickedHandler(event: any): void {
+    for (const item of this.cells) {
+      item.style.overflow = 'hidden';
+    }
+    this.state = !this.state;
+
+    if (this.state) {
+      event.currentTarget.parentNode.parentNode.parentNode.style.overflow = 'visible';
+    } else {
+      event.currentTarget.parentNode.parentNode.parentNode.style.overflow = 'hidden';
+    }
+    console.log('STATE', this.state);
+
     const target = event.target.getAttribute('btntype');
     if (target) {
       this.params.clicked(target);
@@ -23,7 +37,8 @@ export class UserBlockControllComponent implements ICellRendererAngularComp, OnD
     }
   }
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnDestroy(): void {
   }
